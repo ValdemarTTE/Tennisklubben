792924696ef21064aed9bf9986b94c250ca2e1ca
SimonM32 <simi1002@stud.ek.dk>
2026-05-07 11:25:50 +0200

    feature: menu ui (Simin)

--- src/main/java/ui/MainMenu.java
+++ src/main/java/ui/MainMenu.java
@@ -1,4 +1,46 @@
 package ui;
 
+import java.util.Scanner;
+
 public class MainMenu {
+
+    Scanner scanner = new Scanner(System.in);
+
+    ChairmanMenu menuA = new ChairmanMenu();
+    CashierMenu menuB = new CashierMenu();
+    CoachMenu menuC = new CoachMenu();
+
+    public void run() {
+
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Velkommen til tennisklubbens hovedmenu. Vælg 1 - 4: ");
+            System.out.println("1. Formanden menu");
+            System.out.println("2. kasserer menu");
+            System.out.println("3. Træner menu");
+            System.out.println("4. Afslut program");
+
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    menuA.startChairmanMenu();
+                    break;
+
+                case 2:
+                    menuB.startCashierMenu();
+                    break;
+
+                case 3:
+                    menuC.startCoachMenu();
+                    break;
+
+                case 4:
+                    running = false;
+                    break;
+            }
+        }
+    }
 }
