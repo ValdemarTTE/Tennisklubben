9ad71429c6ef30418216be977e5f9343c33bb4da
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-05-11 14:07:30 +0200

    ILogger (Interface)

--- src/main/java/interfaces/ILogger.java
+++ src/main/java/interfaces/ILogger.java
@@ -1,4 +1,10 @@
 package interfaces;
+import java.time.LocalDateTime;
 
 public interface ILogger {
+    void logInfo( String message);
+    void logWarning(String message);
+    void logError(String message, Exception e);
+    void clearLog();
+    LocalDateTime timestampFormat();
 }
