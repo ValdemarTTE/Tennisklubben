ff1c43f91158eb8543002484b06b2c6429f2be0f
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-05-11 14:00:07 +0200

    enum LogLevel

--- src/main/java/logger/LogLevel.java
+++ src/main/java/logger/LogLevel.java
@@ -1,4 +1,18 @@
 package logger;
 
 public enum LogLevel {
+    INFO ("Info"),
+    WARNING("Warning"),
+    ERROR ("Error");
+
+    private final String label;
+
+    LogLevel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
 }
