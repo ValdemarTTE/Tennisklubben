ea468612645a4c7f6d5bbcacc6e70cdc68cd4d06
nadir77 <nahi1000@stud.ek.dk>
2026-05-11 14:36:58 +0200

    3 klasser active,passive,finans

--- src/main/java/model/ActiveMember.java
+++ src/main/java/model/ActiveMember.java
@@ -1,4 +1,18 @@
 package model;
 
-public class ActiveMember {
+public class ActiveMember extends Member {
+    private boolean isCompetitive;
+
+    public ActiveMember(int memberId, String name, int age, MemberType memberType, boolean isCompetitive) {
+        super(memberId, name, age, MemberType);
+        this.isCompetitive = isCompetitive;
+    }
+
+    public boolean isCompetitive() {
+        return isCompetitive;
+    }
+
+    public void setCompetitive(boolean competitive) {
+        isCompetitive = competitive;
+    }
 }
--- src/main/java/model/PassiveMember.java
+++ src/main/java/model/PassiveMember.java
@@ -1,4 +1,13 @@
 package model;
 
-public class PassiveMember {
+public class PassiveMember extends Member {
+    public PassiveMember(int memberId, String name) {
+        super(memberId, name, MemberType.PASSIV);
+        System.out.println("PassivMember created:" + name);
+    }
+    @Override
+    public double calculateFee() {
+        return 250;
+    }
 }
+
