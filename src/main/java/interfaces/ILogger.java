ad5da56697a575b51594f6331664970e6397a36b
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-05-11 14:09:43 +0200

    ILogger (Interface) + Log_file

--- src/main/java/interfaces/ILogger.java
+++ src/main/java/interfaces/ILogger.java
@@ -2,6 +2,7 @@ package interfaces;
 import java.time.LocalDateTime;
 
 public interface ILogger {
+    String LOG_FILE_PATH = "application.log";
     void logInfo( String message);
     void logWarning(String message);
     void logError(String message, Exception e);
