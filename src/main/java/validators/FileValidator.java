bcd0f8b2d342e758eff26ad09d69dbde3a971716
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-05-12 11:04:41 +0200

    Methods in FileValidator

--- src/main/java/validators/FileValidator.java
+++ src/main/java/validators/FileValidator.java
@@ -1,9 +1,40 @@
 package validators;
 
-public class FileValidator {
-boolean validate (Object obj){
+import java.io.File;
 
-    return false;
+public class FileValidator implements IValidator<File> {
+    private String errorMessage;
+
+    @Override
+    public boolean validate(File file) {
+
+        if (file == null) {
+            errorMessage = "File cannot be null";
+            return false;
+        }
+
+        if (!file.exists()) {
+            errorMessage = "File does not exist";
+            return false;
+        }
+
+        if (!file.isFile()) {
+            errorMessage = "Invalid file";
+            return false;
+        }
+
+        if (!file.getName().toLowerCase().endsWith(".csv")) {
+            errorMessage = "File must be a CSV file";
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
 
 }
