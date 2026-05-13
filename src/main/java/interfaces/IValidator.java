2314fdb21da451d65017b99fdd2082b38fd6da57
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-05-13 12:01:11 +0200

    remover Exception

--- src/main/java/interfaces/IValidator.java
+++ src/main/java/interfaces/IValidator.java
@@ -1,6 +1,6 @@
 package interfaces;
 //<T> reusable for many object types, type-safe, avoids unnecessary casting
 public interface IValidator <T> {
-boolean validate (T obj) throws Exception;
+boolean validate (T obj);
 String getErrorMessage();
 }
