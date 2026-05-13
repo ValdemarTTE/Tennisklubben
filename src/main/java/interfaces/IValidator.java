8905d01d20f0c511e3bc54c5ec744572575b5935
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-05-13 11:59:30 +0200

    changed if in the method

--- src/main/java/interfaces/IValidator.java
+++ src/main/java/interfaces/IValidator.java
@@ -1,6 +1,6 @@
 package interfaces;
 //<T> reusable for many object types, type-safe, avoids unnecessary casting
 public interface IValidator <T> {
-boolean validate (T obj);
+boolean validate (T obj) throws Exception;
 String getErrorMessage();
 }
